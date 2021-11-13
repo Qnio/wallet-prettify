7aab58c7cfe82947dc42b1c6acd95fd2178c2e63
Piotr Kuniniec <peter@digitalpurpose.com.au>
2021-11-14 09:18:55 +1100

    Creating main components

--- src/AddressPrettify.tsx
+++ src/AddressPrettify.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import './AddressPettify.css';
 
 export interface WalletAddress {
     address: string,
-    tileWidth: string,
-    tileHeight: string
+    tileWidth?: string,
+    tileHeight?: string
 }
 
-export const AddressPrettify = ({address, tileWidth, tileHeight}: WalletAddress) => {
+export const AddressPrettify = ({address, tileWidth = '100%', tileHeight = '5px'}: WalletAddress) => {
     const changeLeadingTwoSigns = address.replace(/^.{2}/g, '00');
     const addressToColourArray = changeLeadingTwoSigns.split(/(?<=^(?:.{6})+)(?!$)/);
 
     return (
-        <div className='address-container'>
+        <div style={{display: 'flex', maxWidth: 'fit-content', flexDirection: 'column'}}>
             <div>
                 {address}
             </div>
-            <div className='display-address-block'>
+            <div style={{display: 'flex'}}>
                 {addressToColourArray.map(element => {
                     const tile = {
                         backgroundColor: `#${element}`,
